468d1420e54a0d41f2f6874670c3c2e6b732dc72
yesacs <casey.sullivan@gmail.com>
2015-06-29 17:02:50 -0400

    Added education data, added readme, removed loads from footer

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# resume
+My resume, in React form
+
+    npm install
+    gulp
+
+(maybe)
+
+http://yesacs.github.io/resume/
--- src/js/footer.jsx
+++ src/js/footer.jsx
@@ -1,6 +1,5 @@
 'use strict';
 import React from 'react';
-import _ from 'lodash';
 
 export class Footer extends React.Component {
     constructor(props) {
