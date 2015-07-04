5cd54571a50a2b6af0261055af06c2f4fc28ed02
yesacs <casey.sullivan@gmail.com>
2015-07-04 14:30:52 -0400

    Added uglification
     and some better looking, but temporary, browserify errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,47 @@
 /////////////////////////////
 
 
-var gulp       = require('gulp'),
-    sass       = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    eslint     = require('gulp-eslint'),
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    eslint        = require('gulp-eslint'),
+    eslintStylish = require('eslint-stylish'),
+    gutil         = require('gulp-util'),
 
-    browserify = require('browserify'),
-    watchify   = require('watchify'),
-    babelify   = require('babelify'),
-    reactify   = require('reactify'),
+    browserify    = require('browserify'),
+    watchify      = require('watchify'),
+    babelify      = require('babelify'),
+    reactify      = require('reactify'),
 
-    source     = require('vinyl-source-stream'),
-    buffer     = require('vinyl-buffer');
+    source        = require('vinyl-source-stream'),
+    buffer        = require('vinyl-buffer'),
+
+    uglify        = require('gulp-uglify'),
+    chalk         = require('chalk');
+
+
+var compressOptions = {
+        'sequences': true,
+        'properties': true,
+        'dead_code': true,
+        'drop_debugger': true,
+        'unsafe': false,
+        'conditionals': true,
+        'comparisons': true,
+        'evaluate': true,
+        'booleans': true,
+        'loops': true,
+        'unused': true,
+        'hoist_funs': true,
+        'hoist_vars': false,
+        'if_return': true,
+        'join_vars': true,
+        'cascade': true,
+        'side_effects': true,
+        'warnings': false,
+        'drop_console': true,
+        'global_defs': {}
+    };
 
 
 /////////////////////////////
@@ -20,8 +49,8 @@ var gulp       = require('gulp'),
 
 gulp.task('sass', function () {
     gulp.src('./src/scss/**/*.scss')
-        .pipe(sourcemaps.write())
         .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 });
 
@@ -43,7 +72,9 @@ var jsFiles = [
     ],
     linter = function () {
         return gulp.src(jsFiles)
-                   .pipe(eslint())
+                   .pipe(eslint({
+                       format: eslintStylish
+                   }))
                    .pipe(eslint.format())
                    .pipe(eslint.failOnError());
     };
@@ -53,30 +84,55 @@ gulp.task('lint', linter);
 
 /////////////////////////////
 
+// via https://gist.github.com/Fishrock123/8ea81dad3197c2f84366
+var mapError = function(err) {
+  if (err.fileName) {
+    // regular error
+    gutil.log(chalk.red(err.name)
+      + ': '
+      + chalk.yellow(err.fileName.replace(__dirname + '/src/js/', ''))
+      + ': '
+      + 'Line '
+      + chalk.magenta(err.lineNumber)
+      + ' & '
+      + 'Column '
+      + chalk.magenta(err.columnNumber || err.column)
+      + ': '
+      + chalk.blue(err.description));
+  } else {
+    // browserify error..
+    gutil.log(chalk.red(err.name)
+      + ': '
+      + chalk.yellow(err.message));
+  }
+
+  process.exit(1);
+};
+/* */
 
 var compile = function (shouldWatch, shouldSourceMap) {
     var bundler = watchify(browserify('./src/js/app.jsx', { debug: true }).transform(babelify).transform(reactify));
 
-    function rebundle() {
-        bundler.bundle()
-               .on('error', function(err) { console.error(err); this.emit('end'); })
-               .pipe(source('build.js'))
-               .pipe(buffer())
-               .pipe(sourcemaps.init({ loadMaps: shouldSourceMap || false }))
-               .pipe(sourcemaps.write('./'))
-               .pipe(gulp.dest('./dist/js'));
-    }
+    var rebundle = function() {
+        return bundler.bundle()
+                      .on('error', mapError)
+                      .pipe(source('build.js'))
+                      .pipe(buffer())
+                      .pipe(sourcemaps.init({ loadMaps: shouldSourceMap || false }))
+                          .pipe(uglify({
+                              compress: compressOptions
+                          }))
+                      .pipe(sourcemaps.write('./'))
+                      .pipe(gulp.dest('./dist/js'));
+    };
 
     if (shouldWatch) {
-        linter();
-        bundler.on('update', function() {
-            console.log('-> rebundling...');
+        return bundler.on('update', function() {
             rebundle();
-            console.log('-> rebulded ');
         });
     }
 
-    rebundle();
+    return rebundle();
 };
 
 var watch = function () {
